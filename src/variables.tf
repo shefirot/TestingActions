6225a820d3ed8ca9ffd928c820e67a174924adbf
Cloud User <ec2-user@ip-172-31-35-68.ap-northeast-3.compute.internal>
2023-04-25 15:44:13 +0000

    Push lot GH PAT

---
+++ src/variables.tf
@@ -0,0 +1,10 @@
+variable "git_fake_pat" {
+  type        = string
+  description = "Fake PAT to alert with gitleaks"
+  default     = "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"
+}
+variable "git_another_fake_pat" {
+  type        = string
+  description = "Fake PAT to alert with gitleaks"
+  default     = "ghp_sfemPLOW1KU9qH7uHa2VL4IvrG8Scq7829Dq"
+}
